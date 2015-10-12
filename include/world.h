c3a43d31c1424246b9b659da1203a7722d4e8235
William Casarin <bill@casarin.me>
2015-10-12 16:24:28 -0700

    world.h: cleanup decls

--- include/world.h
+++ include/world.h
@@ -43,9 +43,6 @@ struct world {
   int h;
 };
 
-void
-world_clear (struct world *world);
-
 struct pixel*
 world_get_pixel_ind (struct world *world, int code, int x, int y);
 
@@ -55,19 +52,17 @@ init_pixel_at (struct world *world, int x, int y, u8 type, float accel);
 struct pixel*
 world_get_pixel (struct world *world, int x, int y);
 
-void
-world_randomize (struct world *world);
-
 struct world *
 world_init(struct world *world, int w, int h);
 
-void
-world_swap_buffers(struct world *world);
+void world_describe_rle(struct world *world,
+                        struct pixel *pixels, int ncols);
 
-void
-world_update(struct world *world);
-
-void
-world_free(struct world *world);
+void world_clear (struct world *world);
+void world_randomize (struct world *world);
+void world_swap_buffers(struct world *world);
+void world_update(struct world *world);
+void world_update_automata(struct world *world);
+void world_free(struct world *world);
 
 #endif /* SANDY_WORLD_H */
